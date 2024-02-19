592b6b219293684e7bee16f2f90dd05a059bbb0e
SummCoder <2404174187@qq.com>
2024-02-19 21:36:14 +0800

    初始化项目结构

--- src/main/java/miniblog/data/Repository.java
+++
@@ -1,9 +0,0 @@
-package miniblog.data;
-
-import miniblog.Contact;
-import org.springframework.data.repository.CrudRepository;
-
-public interface Repository
-        extends CrudRepository<Contact, Long> {
-
-}
