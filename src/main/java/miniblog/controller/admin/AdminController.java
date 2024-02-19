a149a5f1d2d9df820c620105c5ce7ae3be874e7f
SummCoder <2404174187@qq.com>
2024-02-19 22:08:03 +0800

    限制文件模板

--- src/main/java/miniblog/controller/admin/AdminController.java
+++ src/main/java/miniblog/controller/admin/AdminController.java
@@ -3,8 +3,14 @@ package miniblog.controller.admin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @desc Admin Controller.
+ * @author SummCoder
+ * @date 2024/2/19 22:01
+ */
+
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/api/admin")
 public class AdminController {
 
 }
---
+++ src/main/java/miniblog/security/SecurityConfig.java
@@ -0,0 +1,57 @@
+package miniblog.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author SummCoder
+ * @desc  Spring Security.
+ * @date 2024/2/19 22:05
+ */
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic()
+                .and().csrf().disable()
+        ;
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+
+        auth
+                .inMemoryAuthentication()
+                .withUser("buzz")
+                .password("infinity")
+                .authorities("ROLE_USER")
+                .and()
+                .withUser("woody")
+                .password("bullseye")
+                .authorities("ROLE_USER");
+
+    }
+
+}
